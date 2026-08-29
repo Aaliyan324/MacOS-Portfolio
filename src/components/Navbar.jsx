4ff2d2176f84506de10c4787620f6f3c20e04301
Aaliyan <81.aaliyan@gmail.com>
2026-08-29 23:02:14 +0500

    added links to navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { navLinks } from '#constants'
+
+
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src='/images/logo.svg' />
+            <p className='font-bold'>Aaliyan's Portfolio</p>
+
+            <ul>
+                {navLinks.map(({ id, name })=>(
+                    <li key={id}>
+                        <p>{name}</p>
+                    </li> 
+                )
+                )}
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
