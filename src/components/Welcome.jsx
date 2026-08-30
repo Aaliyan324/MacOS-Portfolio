94c48c14056db262624ee6d48300f28d7f35631b
Aaliyan <81.aaliyan@gmail.com>
2026-08-30 22:09:11 +0500

    applied hover function to the letters

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,22 +1,61 @@
 import React, { useRef } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
+const FONT_WEIGHT={
+  subtitle: {min: 100, max:400, default:100},
+  title: {min: 400, max:900, default:400},
+}
 
 const renderText = (text, className, baseWeight = 400) => {
   return [...text].map((char, i) => (
-    <span key={i} className={className} style={{ fontVariationSettings: `'whgt ${baseWeight}` }}>
+    <span key={i} className={className} style={{ fontVariationSettings: `'wght' ${baseWeight}` }}>
       {char === " " ? '\u00A0' : char}
     </span>
   ))
+}
+
+const setupTextHover = (container, type) => {
+  if (!container) return;
+
+  const letters = container.querySelectorAll('span');
+
+  const {min, max, default: base} = FONT_WEIGHT[type];
+
+  const animateLetter = (letter, weight, duration=0.3)=>{
+    return gsap.to(letter, {duration, ease:'power2.out', fontVariationSettings:`'wght' ${weight}`});
+  }
+
+  const handleMouseMove=(e)=>{
+    const { left }=container.getBoundingClientRect();
+    const mouseX=e.clientX - left;
+
+    letters.forEach((letter)=>{
+      const { left: l, width: w } = letter.getBoundingClientRect();
+      const distance = Math.abs(mouseX-(l-left+w/2));
+      const intensity = Math.exp(-(distance**2)/2000)
+
+      animateLetter(letter, min + (max-min)*intensity)
+    })
+  }
+
+  container.addEventListener("mousemove", handleMouseMove)
 }
 
 const Welcome = () => {
   const titleRef = useRef(null)
   const subtitleRef = useRef(null)
 
+  useGSAP(()=>{
+    setupTextHover(titleRef.current, "title");
+    setupTextHover(subtitleRef.current, "subtitle")
+  },[])
+
   return (
     <section id='welcome'>
       <p ref={subtitleRef}>
         {renderText("Hey, I am Aaliyan! Welcome to my", 'text-3xl font-georama', 100)}
-        
+
       </p>
       <h1 ref={titleRef} className='mt-6 uppercase'>
         {renderText("Portfolio", 'text-9xl italic font-georama', 400)}
